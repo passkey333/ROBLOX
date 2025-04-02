994f51dc9271ce49431ad5cb51dfe6c0cabf76e5
passkey333 <brorealman@gmail.com>
2025-04-02 18:12:33 +0500

    Update Admin kolsh.lua

--- Admin kolsh.lua
+++ Admin kolsh.lua
@@ -1,7 +1,7 @@
-local owners = {"NikepRro2004"} -- Are able to set admins who can ban/etc... using :pa name
-local admins = {"NikePro2004"} -- Sets admins who can use ban/kick/admin or shutdown
+local owners = {"hl_kill"} -- Are able to set admins who can ban/etc... using :pa name
+local admins = {"hl_kill"} -- Sets admins who can use ban/kick/admin or shutdown
 local tempadmins = {} -- Sets admins who can't use ban/kick/admin or shutdown
-local banland = {"isik"} -- Permanently Bans people
+local banland = {"hl_kill"} -- Permanently Bans people
 local prefix = ":" -- If you wanna change how your commands start ':'kill noob
 local AutoUpdate = true -- Set to false if you don't want it to automatically update
 local FunCommands = true -- Set to false if you only want the basic commands (For Strict Places)
